77dc2f7f71c1fb0110f411fa614adb754ac3ea57
AbdulRaufHassan <raufh1093@gmail.com>
2024-02-24 21:05:00 +0500

    add authentication

--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,9 @@
 import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyC49QFBvQR562OsyLer-9DdyHr8gBdnGwQ",
@@ -6,7 +11,10 @@ const firebaseConfig = {
   projectId: "chat-app-reactjs-c1f27",
   storageBucket: "chat-app-reactjs-c1f27.appspot.com",
   messagingSenderId: "954287926483",
-  appId: "1:954287926483:web:b2507791f6dedb6003b826"
+  appId: "1:954287926483:web:b2507791f6dedb6003b826",
 };
 
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export { auth, createUserWithEmailAndPassword, signInWithEmailAndPassword };
--- src/pages/SigninPage.jsx
+++ src/pages/SigninPage.jsx
@@ -2,19 +2,64 @@ import React from "react";
 import "../css/signin_signup_page.css";
 import CHAT_ICON from "../assets/images/chat_icon.svg";
 import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import { auth, signInWithEmailAndPassword } from "../config";
 
 function SigninPage() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    reset,
+    formState: { errors },
+  } = useForm();
+
+  const signin = (data) => {
+    signInWithEmailAndPassword(auth, data.email, data.password)
+      .then((userCredential) => {
+        console.log(userCredential);
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+    reset();
+  };
+
   return (
-    <div className="flex items-center justify-center bg-blue-900 min-h-screen max-h-fit pt-28 pb-4 signin_main_Div">
-      <div className="bg-gray-50 flex flex-col p-4 items-center inner_Div">
+    <div className="flex items-center justify-center min-h-screen max-h-fit pt-7 pb-4 signin_main_Div">
+      <div className="bg-gray-50 flex flex-col px-4 pb-4 items-center inner_Div">
         <img src={CHAT_ICON} />
         <h1 className="text-slate-600 font-black text-5xl h-auto">Signin</h1>
-        <input type="text" placeholder="Email" />
-        <input type="text" placeholder="Password" />
-        <button>Signin</button>
+        <form onSubmit={handleSubmit(signin)}>
+          <input
+            type="text"
+            className={
+              errors.email &&
+              "border-2 border-red-800 focus:border-2 focus:border-red-800"
+            }
+            placeholder="Email"
+            {...register("email", { required: true })}
+          />
+          {errors.email && (
+            <h6 className="text-red-800	mb-4 mx-4 font-medium">Required</h6>
+          )}
+          <input
+            type="password"
+            className={
+              errors.password &&
+              "border-2 border-red-800 focus:border-2 focus:border-red-800"
+            }
+            placeholder="Password"
+            {...register("password", { required: true })}
+          />
+          {errors.password && (
+            <h6 className="text-red-800	mb-4 mx-4 font-medium">Required</h6>
+          )}
+          <button type="submit">Signin</button>
+        </form>
         <span className="text-slate-600 mb-5 text-lg">
           Don't have an account?
-          <Link to={"/"} className="text-indigo-700 ml-2">
+          <Link to={"/"} className="ml-2">
             Signup
           </Link>
         </span>
