e07ed3126ef41894b55ee4a686204916863037ba
AbdulRaufHassan <raufh1093@gmail.com>
2024-03-06 23:07:16 +0500

    improvements in chat page

--- src/pages/ChatPage.jsx
+++ src/pages/ChatPage.jsx
@@ -29,6 +29,7 @@ function ChatPage() {
   const [messageInputVal, setMessageInputVal] = useState("");
   const [allMessages, setAllMessages] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [currentContact, setCurrentContact] = useState({});
 
   const getAllContacts = async () => {
     const currentUserRef = doc(db, "users", auth.currentUser.uid);
@@ -49,6 +50,10 @@ function ChatPage() {
     console.log("get contacts render");
   }, [loading]);
 
+  useEffect(() => {
+    // get messages
+  }, [currentContact]);
+
   const sendMsg = () => {
     setAllMessages([
       ...allMessages,
@@ -64,7 +69,9 @@ function ChatPage() {
       <section className="bg-blue-950 border-r border-gray-400 contactListSec">
         <header className="w-full">
           <div className="w-full bg-slate-300 border-r border-gray-400 p-2 flex items-center justify-between box-border">
-            <div className="h-14 w-14 rounded-full bg-blue-950"></div>
+            <div className="h-14 w-14 rounded-full bg-blue-950 text-slate-300 flex items-center justify-center font-semibold text-2xl roboto-font">
+              {currentUserDoc.fullName?.charAt(0).toUpperCase()}
+            </div>
             <div className="flex items-center">
               <button
                 className="flex flex-col items-center"
@@ -106,7 +113,7 @@ function ChatPage() {
             <div className="w-full flex items-center justify-center px-2 box-border  border-b border-gray-400">
               <input
                 type="text"
-                className="border border-slate-300 bg-transparent my-2 py-2 px-3 w-11/12 rounded-lg text-slate-300 text-lg focus:outline-none placeholder:text-slate-300"
+                className="josefin-font border border-slate-300 bg-transparent my-2 py-2 px-3 w-11/12 rounded-lg text-slate-300 text-lg focus:outline-none placeholder:text-slate-300"
                 placeholder="Search Contact"
               />
             </div>
@@ -118,30 +125,59 @@ function ChatPage() {
           </div>
         ) : allContacts.length ? (
           <ul className="contact_list mt-2">
-            {allContacts.map((contact) => (
-              <li
-                key={contact.uid}
-                className="w-full h-20 flex items-center pl-2 pr-4 box-border hover:bg-slate-500"
-                onClick={() => setCurrentChat(contact)}
-              >
-                <div className="h-14 w-14 rounded-full bg-slate-300"></div>
-                <div className="ml-4 h-full border-t border-slate-500 flex justify-between flex-1">
-                  <div className="flex-1 mt-4">
-                    <h1 className="roboto-font font-semibold text-slate-300 text-xl tracking-wider">
-                      {contact.fullName}
-                    </h1>
-                    <p className="josefin-font text-gray-400">
-                      Lorem, ipsum dolor sit elit...
-                    </p>
+            {allContacts.map((contact) => {
+              let contactIdMatch = currentContact.uid == contact.uid;
+              return (
+                <li
+                  key={contact.uid}
+                  className={`w-full h-20 flex items-center pl-2 pr-4 box-border cursor-pointer ${
+                    contactIdMatch ? "bg-slate-300" : "hover:bg-slate-500"
+                  }`}
+                  onClick={() => setCurrentContact(contact)}
+                >
+                  <div
+                    className={`h-14 w-14 rounded-full flex items-center justify-center roboto-font text-2xl font-semibold ${
+                      contactIdMatch
+                        ? "bg-blue-950 text-slate-300"
+                        : "bg-slate-300 text-blue-950"
+                    }`}
+                  >
+                    {contact.fullName?.charAt(0).toUpperCase()}
                   </div>
-                  <div className="w-auto mt-4">
-                    <span className="inline-block text-gray-400 roboto-font">
-                      4:48 PM
-                    </span>
+                  <div
+                    className={`ml-4 h-full border-t ${
+                      contactIdMatch ? "border-slate-300" : "border-slate-500"
+                    } flex justify-between flex-1`}
+                  >
+                    <div className="flex-1 mt-4">
+                      <h1
+                        className={`roboto-font font-semibold ${
+                          contactIdMatch ? "text-blue-950" : "text-slate-300"
+                        } text-xl tracking-wider`}
+                      >
+                        {contact.fullName}
+                      </h1>
+                      <p
+                        className={`josefin-font ${
+                          contactIdMatch ? "text-gray-500" : "text-gray-400"
+                        }`}
+                      >
+                        Lorem, ipsum dolor sit elit...
+                      </p>
+                    </div>
+                    <div className="w-auto mt-4">
+                      <span
+                        className={`inline-block ${
+                          contactIdMatch ? "text-gray-500" : "text-gray-400"
+                        } roboto-font`}
+                      >
+                        4:48 PM
+                      </span>
+                    </div>
                   </div>
-                </div>
-              </li>
-            ))}
+                </li>
+              );
+            })}
           </ul>
         ) : (
           <div className="w-4/5 mx-auto flex flex-col justify-center items-center text-xl josefin-font text-slate-300 zeroContactMsg">
@@ -152,17 +188,19 @@ function ChatPage() {
       </section>
       <section
         className={`${
-          allContacts.length
+          Object.keys(currentContact).length
             ? "bg-slate-400"
             : "bg-slate-300 flex justify-center items-center"
         } flex-1 MessageSec relative`}
       >
-        {allContacts.length ? (
+        {Object.keys(currentContact).length > 0 ? (
           <>
             <header className="w-full bg-slate-300 flex items-center">
-              <div className="h-14 w-14 rounded-full bg-blue-950 ml-4 mr-3"></div>
+              <div className="h-14 w-14 rounded-full bg-blue-950 ml-4 mr-3 flex items-center justify-center text-slate-300 roboto-font text-2xl font-semibold">
+                {currentContact.fullName?.charAt(0).toUpperCase()}
+              </div>
               <h1 className="roboto-font font-semibold text-blue-950 text-xl tracking-wider">
-                Contact Name
+                {currentContact.fullName}
               </h1>
             </header>
             <section className="flex flex-col-reverse pt-5 py-10 px-3 box-border allMsgsParentDiv">
@@ -170,7 +208,7 @@ function ChatPage() {
                 <div className="flex items-end self-end msg_parent_div">
                   <div
                     key={i}
-                    className="min-h-32 max-h-fit min-w-60 max-w-fit bg-slate-300 flex justify-center items-center msg_style"
+                    className="relative min-h-32 max-h-fit min-w-60 max-w-fit mr-7 bg-slate-300 flex justify-center items-center msg_style"
                   >
                     <p className="z-50 w-4/5 h-4/5 text-center josefin-font">
                       {v.message}
